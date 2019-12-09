43442dff904ec616656b2543a3128d135c024ece
Alejandro Millan <alejandro_millan@ymail.com>
2019-12-08 23:26:28 -0800

    update

--- bot.js
+++ bot.js
@@ -7,82 +7,81 @@ logger.add(logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-
-const client = new Discord.Client();
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
+// Initialize Discord Bot
+var bot = new Discord.Client({
+    token: process.env.BOT_TOKEN,
+    autorun: true
 });
-
-client.on('message', function (user, userID, channelID, message, evt) {
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     var ran = Math.floor(Math.random() * 6);
     message = message.toLowerCase();
     if (message.includes("?")) {
-      bot.sendMessage({
-         to: channelID,
-         message: 'Any other excellent questions'
-      });
-     }
-     if(message.includes("pumping lemma")){
         bot.sendMessage({
-           to: channelID,
-           message: 'DRINK!'
+            to: channelID,
+            message: 'Any other excellent questions'
         });
-     }
-     if(message.includes("mission failed")){
+    }
+    if(message.includes("pumping lemma")){
+        bot.sendMessage({
+            to: channelID,
+            message: 'DRINK!'
+        });
+    }
+    if(message.includes("mission failed")){
         bot.uploadFile({
-           to: channelID,
-           file: "mission.mp3"
+            to: channelID,
+            file: "mission.mp3"
         });
-     }
-     if(message.includes("study guide")){
+    }
+    if(message.includes("study guide")){
         bot.uploadFile({
-           to: channelID,
-           file: "2019-05-15_12-39.pdf"
+            to: channelID,
+            file: "2019-05-15_12-39.pdf"
         });
-     }
-     if(message.includes("motivation")) {
+    }
+    if(message.includes("motivation")) {
         if(ran == 0){
-           bot.uploadFile({
-              to: channelID,
-              file: "positive.png"
-           });
+            bot.uploadFile({
+                to: channelID,
+                file: "positive.png"
+            });
         }
         else if(ran == 1){
-           bot.uploadFile({
-              to: channelID,
-              file: "positive2.jpg"
-           });
+            bot.uploadFile({
+                to: channelID,
+                file: "positive2.jpg"
+            });
         }
         else if(ran == 2){
-           bot.uploadFile({
-              to: channelID,
-              file: "positive3.jpg"
-           });
+            bot.uploadFile({
+                to: channelID,
+                file: "positive3.jpg"
+            });
         }
         else if(ran == 3){
-           bot.uploadFile({
-              to: channelID,
-              file: "positive4.jpg"
-           });
+            bot.uploadFile({
+                to: channelID,
+                file: "positive4.jpg"
+            });
         }
         else if(ran == 4){
-           bot.uploadFile({
-              to: channelID,
-              file: "positive5.png"
-           });
+            bot.uploadFile({
+                to: channelID,
+                file: "positive5.png"
+            });
         }
         else if(ran == 5){
-           bot.uploadFile({
-              to: channelID,
-              file: "positive6.jpg"
-           });
+            bot.uploadFile({
+                to: channelID,
+                file: "positive6.jpg"
+            });
         }
-     }
+    }
 });
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
